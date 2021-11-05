a315329263f652cbb4927f6e36a61b3c0ccf3b8f
Latifah AlMulhim <L-n-a-m@hotmail.com>
2021-11-05 15:21:51 +0300

    notification

--- app/src/main/java/com/example/tasktimerapp/broadcastReceiver/AlarmBroadcastReceiver.kt
+++ app/src/main/java/com/example/tasktimerapp/broadcastReceiver/AlarmBroadcastReceiver.kt
@@ -1,14 +1,10 @@
 package com.example.tasktimerapp.broadcastReceiver
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
+
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.tasktimerapp.R
@@ -16,47 +12,25 @@ import com.example.tasktimerapp.fragments.ViewTaskFragment
 
 
 class AlarmBroadcastReceiver: BroadcastReceiver() {
-    private val channelId = "myapp.notifications"
-    private val description = "Notification App Example"
-    private lateinit var builder:NotificationCompat.Builder
-
+    private val channelId = "taskTimerNotifications"
     override fun onReceive(context: Context?, intent: Intent?) {
-        val i = Intent(context, ViewTaskFragment::class.java)
-        val notificationManager= NotificationManagerCompat.from(context!!)
-        intent!!.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        val pendingIntent =
-            PendingIntent.getActivity(context, 0, i, 0)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-         builder = NotificationCompat.Builder(context, channelId)
+        val i = Intent(context, ViewTaskFragment::class.java)
+        intent!!.flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0, i, 0)
+
+      val  builder = NotificationCompat.Builder(context!!, channelId)
             .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
-            .setContentIntent(pendingIntent)
             .setContentTitle("Task Timer")
-            .setContentText("Do your task :)")
+            .setContentText("It is the time to do your task :)")
             .setAutoCancel(true)
             .setDefaults(NotificationCompat.DEFAULT_ALL)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-            var notificationChannel = NotificationChannel(
-                channelId,
-                description,
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationManager.createNotificationChannel(notificationChannel)
-        }else{
-            builder = NotificationCompat.Builder(context, channelId)
-                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
-                .setContentIntent(pendingIntent)
-                .setContentTitle("Task Timer")
-                .setContentText("Do your task :)")
-                .setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
+           .setContentIntent(pendingIntent)
+        val notificationManager= NotificationManagerCompat.from(context)
+        notificationManager.notify(123, builder.build())
 
 
-        }
-
-        notificationManager.notify(1234, builder.build())
 
     }
 }
--- app/src/main/java/com/example/tasktimerapp/fragments/AlarmFragment.kt
+++ app/src/main/java/com/example/tasktimerapp/fragments/AlarmFragment.kt
@@ -1,27 +1,127 @@
 package com.example.tasktimerapp.fragments
 
+import android.app.*
+import android.content.Context.ALARM_SERVICE
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.example.tasktimerapp.R
+import com.example.tasktimerapp.broadcastReceiver.AlarmBroadcastReceiver
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import com.google.android.material.timepicker.TimeFormat.CLOCK_12H
+import java.util.*
 
 class AlarmFragment : Fragment() {
+    lateinit var btSelectTime: Button
+    lateinit var btSetAlarm: Button
+    lateinit var btCancelAlarm:Button
+    lateinit var tvAlarmTim: TextView
+    private lateinit var timePiker: MaterialTimePicker
+    private lateinit var calendar: Calendar
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-         val view=inflater.inflate(R.layout.fragment_alarm, container, false)
+        val view = inflater.inflate(R.layout.fragment_alarm, container, false)
+        createNotificationChannel()
+        btSelectTime = view.findViewById(R.id.btSelectTime)
+        btSetAlarm = view.findViewById(R.id.btSetAlarm)
+        tvAlarmTim = view.findViewById(R.id.tvAlarmTim)
+        btCancelAlarm=view.findViewById(R.id.btCancelAlarm)
 
-
-
-
+        btSelectTime.setOnClickListener {
+            showTimPicker()
+        }
+        btSetAlarm.setOnClickListener {
+            SetAlarm()
+        }
+        btCancelAlarm.setOnClickListener {
+            cancelAlarm()
+        }
 
         return view
     }
 
 
+
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "taskTimerNotifications"
+            val name: CharSequence = "taskTimerNotificationsChannel"
+            val description = "Chanel for Alarm Manager"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, name, importance)
+            channel.description = description
+            val notificationManager =
+                requireActivity().getSystemService(AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    fun showTimPicker() {
+        timePiker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Alarm Time")
+            .build()
+        timePiker.show(requireActivity().getSupportFragmentManager(), "taskTimerNotifications")
+        timePiker.addOnPositiveButtonClickListener {
+            if (timePiker.hour > 12) {
+                tvAlarmTim.text = String.format("%2d", timePiker.hour - 12) + " : " + String.format(
+                    "%2d",
+                    timePiker.minute
+                ) + " PM"
+            } else {
+                tvAlarmTim.text = String.format("%2d", timePiker.hour) + " : " + String.format(
+                    "%2d",
+                    timePiker.minute
+                ) + " AM"
+            }
+
+            calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY] = timePiker.hour
+            calendar[Calendar.MINUTE] = timePiker.minute
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
+
+        }
+
+
+    }
+    fun SetAlarm() {
+    alarmManager= requireActivity().getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent=Intent(context, AlarmBroadcastReceiver::class.java)
+        pendingIntent= PendingIntent.getBroadcast(context, 0, intent,0)
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,pendingIntent
+        )
+        Toast.makeText(context,"Alarm set Successfully",Toast.LENGTH_LONG).show()
+
+    }
+    fun cancelAlarm(){
+        alarmManager= requireActivity().getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent=Intent(context, AlarmBroadcastReceiver::class.java)
+        pendingIntent= PendingIntent.getBroadcast(context, 0, intent,0)
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(context,"Alarm Cancelled",Toast.LENGTH_LONG).show()
+        tvAlarmTim.text="You didn't set an alarm yet"
+    }
 }
